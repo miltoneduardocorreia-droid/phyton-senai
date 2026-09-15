da69bee5c33b01812557e37111e52a0401c3d273
miltoneduardocorreia-droid <miltoneduardocorreia@gmail.com>
2026-09-15 14:30:30 -0300

    literais

---
+++ definicao-literal/literais.py
@@ -0,0 +1,14 @@
+# String
+nome = "Milton Araujo"
+
+# Números inteiros e decimais
+idade = 60
+altura = 1.75
+
+# Valor lógico (true e false)
+professor = True 
+# Verifica a tipagem das variáveis
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(professor))
