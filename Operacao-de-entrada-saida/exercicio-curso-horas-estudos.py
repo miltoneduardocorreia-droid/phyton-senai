6e297c3941503b492ca0d0470ac20004df5578bb
miltoneduardocorreia-droid <miltoneduardocorreia@gmail.com>
2026-09-17 15:43:02 -0300

    Carga horária de curso

---
+++ Operacao-de-entrada-saida/exercicio-curso-horas-estudos.py
@@ -0,0 +1,14 @@
+#Faça um programa que peça o nome de um curso
+# e  a quantidade de horas que o aluno estuda nesse curso.
+# Depois, mostre essas informações no terminal.
+
+# Entradas de dados básicos
+nome_curso = (input("Digite o nome do curso: "))
+carga_horaria = float(input("Digite a carga horária: "))
+
+# Saída das informações
+
+print(f"Nome do curso: {nome_curso}")
+print(f"Carga horária: {carga_horaria} horas")
+
+
