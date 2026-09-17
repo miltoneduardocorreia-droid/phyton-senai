766931233f2b522136bf5d4ce37f383105f5dcff
miltoneduardocorreia-droid <miltoneduardocorreia@gmail.com>
2026-09-17 14:04:49 -0300

    soma-numeros

---
+++ Operacao-de-entrada-saida/soma-numeros.py
@@ -0,0 +1,20 @@
+# Entrada de dados básicos
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+# Processamento computacional
+soma = numero1 + numero2
+sub = numero1 - numero2
+div = numero1 / numero2
+mult = numero1 * numero2
+
+print(type(numero1))
+print(type(numero2))
+
+print(f"A soma é: {soma}")
+print(f"A subtração é: {sub}")
+print(f"A divisão é: {div}")
+print(f"A multiplicação é: {mult}")
+
+
+# Saída da informação
