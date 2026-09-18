899e84d9dd8bac27d2d3838873dbeed0804ba70b
miltoneduardocorreia-droid <miltoneduardocorreia@gmail.com>
2026-09-18 14:31:12 -0300

    Login

---
+++ Estruturas-condicionais-e-repeticao/02-login.py
@@ -0,0 +1,12 @@
+# Solicita o usuário
+login = input("Digite seu login: ")
+
+# Solicita a senha do usuário
+senha = input("Digite sua senha: ")
+
+# Verifica se o login está correto e verifica se a senha está correta
+if login == "admin" and senha == "1234":
+    print("Seja bem vindo, administrador!")
+else:
+    print("Login ou senha incorretos!")
+
