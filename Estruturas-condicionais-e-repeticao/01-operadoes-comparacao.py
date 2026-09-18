cd7a8f1c50c0d006b9a144125f47770534d872d6
miltoneduardocorreia-droid <miltoneduardocorreia@gmail.com>
2026-09-18 14:09:45 -0300

    Operadores condicionais

---
+++ Estruturas-condicionais-e-repeticao/01-operadoes-comparacao.py
@@ -0,0 +1,23 @@
+# Entrada de dados
+
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite outro número: "))
+
+# Exibe o resultado da expressão
+# Verifica se os números são iguais
+print("numero1 == numero2: ", numero1 == numero2)
+
+# Verifica se os números são diferentes
+print("numero1 != numero2: ", numero1 != numero2 )
+
+# Verifica se o número 1 é menor que o número 2
+print("numero1 < numero2: ", numero1 < numero2)
+
+# Verifica se o número 1 é maior que o número 2
+print("numero1 > numero2: ", numero1 > numero2)
+
+# Verifica se o número 1 é menor ou igual que o número 2
+print("numero1 <= numero2: ", numero1 <= numero2)
+
+# Verifica se o número 1 é maior ou igual que o número 2
+print("numero1 >= numero2: ", numero1 >= numero2)
