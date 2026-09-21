28d215bcb0bd5f2841e87abe3ec3ce987e11886f
miltoneduardocorreia-droid <miltoneduardocorreia@gmail.com>
2026-09-21 15:57:19 -0300

    Adicionando for

---
+++ 04-Estrutura-de-repeticao/07-tabuada.py
@@ -0,0 +1,12 @@
+# Cria um programa que peça um número ao utilizador e mostre a 
+# tabuada desse número de 1 a 10, utilizando um ciclo for.
+# Exemplo: Digite um número: 5
+    # 5 x 1 = 5
+    # 5 x 2 = 10
+    # 5 x 3 = 15
+    # ...
+    # 5 x 10 = 50
+numero = int(input("Difite um número: "))
+for num in range (1,11):
+   resultado = numero * num
+   print( numero, "x" , num, "=", resultado)
